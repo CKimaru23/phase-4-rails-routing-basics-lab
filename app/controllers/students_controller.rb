0d699f93bc0a61b35099774d18bb081d4e544ff5
Collins Kiragu <collins.kiragu@student.moringaschool.com>
2023-03-14 06:01:13 +0300

    Completed Assignment

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,30 @@
+# app/controllers/students_controller.rb
+class StudentsController < ApplicationController
+    def index
+      students = Student.all
+      render json: students
+    end
+  
+    def grades
+      students = Student.order(grade: :desc)
+      render json: students
+    end
+end# app/controllers/students_controller.rb
+class StudentsController < ApplicationController
+  def index
+    students = Student.all
+    render json: students
+  end
+
+  def grades
+    students = Student.order(grade: :desc)
+    render json: students
+  end
+
+  def highest_grade
+    student = Student.order(grade: :desc).first
+    render json: student
+  end
+end
+
+  
